5a7320dab6a8b696e4dad07f9954f8b17f0f3343
KavishDhiman <kavishdhiman2003@gmail.com>
2026-05-14 12:16:41 +0530

    [Kavish] Added : Initial project structure

---
+++ src/main/java/com/createtask/createtask/CreateTaskApplication.java
@@ -0,0 +1,13 @@
+package com.createtask.createtask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreateTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreateTaskApplication.class, args);
+	}
+
+}

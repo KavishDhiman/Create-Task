305ad55876b7216fab232a984a97be2a13332748
Wave-Shot <vsh.2509@gmail.com>
2026-05-16 11:01:25 +0530

    [Srihari]: Update repo

--- src/main/java/com/createtask/createtask/repository/AttachmentRepository.java
+++ src/main/java/com/createtask/createtask/repository/AttachmentRepository.java
@@ -4,6 +4,14 @@ import com.createtask.createtask.entity.Attachment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface AttachmentRepository extends JpaRepository<Attachment, Integer> {
+
+    List<Attachment> findByFileName(String fileName);
+
+    List<Attachment> findByFilePath(String filePath);
+
+    boolean existsByFileName(String fileName);
 }
--- src/main/java/com/createtask/createtask/repository/CommentRepository.java
+++ src/main/java/com/createtask/createtask/repository/CommentRepository.java
@@ -4,6 +4,15 @@ import com.createtask.createtask.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Integer> {
+
+    List<Comment> findByText(String text);
+
+    List<Comment> findByCreatedAt(LocalDateTime createdAt);
+
+    boolean existsByText(String text);
 }
